7ab8ab723bab4a65c9b3113cf407d1a86945b2c3
FatimaBadran97 <fatima.s.badran@gmail.com>
2024-04-19 10:47:38 +0300

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+module.exports = defineConfig({
+  e2e: {
+    // specPattern: '**/*.{feature,cy.js}',
+    specPattern: '**/*.feature',
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber());
+    },
+    baseUrl: 'https://trello.com/',
+  },
+});
